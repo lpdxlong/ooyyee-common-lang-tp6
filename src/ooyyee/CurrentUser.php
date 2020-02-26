5c845924f859ea527f7445cf7a9351962e501d51
lpdx111 <1581489696@qq.com>
2020-02-26 10:13:02 +0800

    CurrentUser

--- src/ooyyee/CurrentUser.php
+++ src/ooyyee/CurrentUser.php
@@ -6,12 +6,12 @@ use think\facade\Db;
 
 class CurrentUser
 {
-	private $user = array (
-	    'id' => 0,
+    private $user = array (
+        'id' => 0,
         'name'=>'',
-	);
+    );
 
-	public function __construct(){}
+    public function __construct(){}
 
     /**
      * @param $uid
@@ -20,7 +20,7 @@ class CurrentUser
      * @throws \think\db\exception\DbException
      * @throws \think\db\exception\ModelNotFoundException
      */
-	public  function create($uid){
+    public  function create($uid){
 
         $user = cache ( 'login_admin_' . $uid );
         if (! $user) {
@@ -55,27 +55,27 @@ class CurrentUser
                 $user['permissions'] = [ ];
             }
             $user['roles']=$roleIds;
-            cache ( 'login_admin_' . $uid, $user, 7200 );
+            cache ( 'login_admin_' . $uid, json_encode($user), 7200 );
         }
-        $this->user = $user;
-		return $this->user;
-	}
+        $this->user = is_string($user)?json_decode($user,true):$user;
+        return $this->user;
+    }
 
-	public  function isLogin():bool {
-		return $this->uid() ? true : false;
-	}
+    public  function isLogin():bool {
+        return $this->uid() ? true : false;
+    }
 
-	public  function isSuperAdmin(){
-		$user=$this->user;
-		return  $user['super']?? 0;
-	}
-	public  function user():array {
-		return $this->user;
-	}
-	public  function uid(){
-    $user = $this->user;
-    return $user['id'];
-}
+    public  function isSuperAdmin(){
+        $user=$this->user;
+        return  $user['super']?? 0;
+    }
+    public  function user():array {
+        return $this->user;
+    }
+    public  function uid(){
+        $user = $this->user;
+        return $user['id'];
+    }
 
     /**
      * @return string
@@ -84,17 +84,17 @@ class CurrentUser
         $user =$this->user;
         return $user['name'];
     }
-	public  function permissions(){
-		$user =$this->user;
-		return $user['permissions']?? [];
-	}
-	public  function hasRole($roleId):bool {
+    public  function permissions(){
+        $user =$this->user;
+        return $user['permissions']?? [];
+    }
+    public  function hasRole($roleId):bool {
         $user = $this->user;
         return isset ( $user['roles'] ) ? in_array($roleId,$user['roles'],false) : false;
     }
-	public  function hasPermission($permissionId):bool {
-		$permissions=$this->permissions();
-		return in_array($permissionId, $permissions,false);
-	}
+    public  function hasPermission($permissionId):bool {
+        $permissions=$this->permissions();
+        return in_array($permissionId, $permissions,false);
+    }
 }
 
